0adc286f54aff83297ada7b68ed3cfb439a801ab
Andres Carvajal <jc1993box@hotmail.com>
2019-11-06 02:30:52 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,16 @@
+
+
+
 # birthday_kids = {
 #   "Timmy" => 9, 
 #   "Sarah" => 6, 
 #   "Amanda" => 27
 # }
-
+require "pry"
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+
+
+binding.pry
+"birthday"
 end
-
-
