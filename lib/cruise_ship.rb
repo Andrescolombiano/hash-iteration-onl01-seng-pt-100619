94b64f969b300bff6ad4e08b703fe7d2fa80ec3b
Andres Carvajal <jc1993box@hotmail.com>
2019-11-06 02:34:34 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -5,15 +5,12 @@
 # suite_d: "Charlie Chaplin", 
 # suite_e: "Crumpet the Elf"
 # }
-require "pry"
+
 def select_winner(passengers)
 winner = ""
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
     winner = name
-    
-binding.pry
-"birthday"
   end
 end
 
